1ce5bcefa2584c3515c5a8d7bd5ae49dd68ae854
Damini Varu <damvaru4@gmail.com>
2019-09-25 20:37:23 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,4 @@
-def say_hello
-  print "hello #{say_hello}"
-end 
-
- # Build your say_hello method here
- 
- say_hello = ("Gabriela")
+def say_hello('name')
+  name = 'bob'
+  print "Hello, #{name}!"
+end
